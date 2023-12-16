c470b4d977d4a93962df7b6f6806ebe94cf68f7c
eva <admin@yahoo.com>
2023-12-16 17:53:27 -0600

    ntitc

---
+++ src/practice/study.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class study {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		study pen = new study();
+		pen.java();
+		
+		
+		
+		
+		
+	}
+	
+	public void java() {
+    double a = 10.22;
+    double b = 20.99;
+    double c = a+b;
+	System.out.println(c);	
+		
+	}
+	
+	
+	
+
+}
